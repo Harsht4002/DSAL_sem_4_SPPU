e7e2be0cc449d819c9e12738b015b63878d515b2
Harshwardhan Thube <139372754+Harsht4002@users.noreply.github.com>
2024-05-05 13:27:39 +0530

    Update dsal_A4.py

--- dsal_A4.py
+++ dsal_A4.py
@@ -1,68 +1,60 @@
 # To create ADT that implement the "set" concept. a. Add (new Element) -Place a value into the set , b. Remove (element) Remove the value 
 # c. Contains (element) Return true if element is in collection, d. Size () Return number of values in collection Iterator () Return an 
 # iterator used to loop over collection, e. Intersection of two sets , f. Union of two sets, g. Difference between two sets, h. Subset
-
 class Set:
-    def __init__(self) -> None:
-        self.data = {}
-    
-    def add(self, element):
-        self.data[element] = True
-        
-    def remove(self, element):
-        if element in self.data:
-            del self.data[element]
-    
-    def contains(self, element):
-        return element in self.data
-    
+    def __init__(self):
+        self.data=[]
+    def add(self,no):
+        if no not in self.data:
+            self.data.append(no)
+    def remove(self,no):
+        if no in self.data:
+            self.data.remove(no)
+    def contain(self,no):
+        if no in self.data:
+            return True
+        return False
     def size(self):
         return len(self.data)
-    
+    def intersection(self,set2):
+        res=set()
+        for i in self.data:
+            if i in set2.data:
+                res.add(i)
+        return res
+    def union(self,set2):
+        res=set()
+        for i in self.data:
+            res.add(i)
+        for i in set2.data:
+            res.add(i)
+        return res
+    def difference(self,set2):
+        res=set()
+        for i in self.data:
+            if not set2.contain(i):
+                res.add(i)
+        for i in set2.data:
+            if not self.contain(i):
+                res.add(i)
+        return res        
+    def subset(self,set2):
+        res=set()
+        for i in set2.data:
+            if not self.contain(i):
+                return False
+        return True
     def __iter__(self):
-        return iter(self.data.keys())
-    
-    def intersection(self, other_set):
-        dict = {}
-        for key in self.data:
-            if key in other_set.data:
-                dict[key]=True
-        return tuple([i for i in dict])           
-    
-    def union(self, other_set):
-        dict = {}
-        for i in self.data:
-            dict[i]=True
-        for i in other_set.data:
-            dict[i]=True
-        return tuple([i for i in dict])
-        
-    def difference(self, other_set):
-        dict = {}
-        for i in self.data:
-            if i not in other_set.data:
-                dict[i]=True
-        return tuple([i for i in dict])
-        
-    def subset(self, other_set):
-        return all(element in other_set.data for element in self.data)
-    
-A = Set()
-B = Set()
-A.add(32)
-A.add(24)
-A.add(2)
-A.add(0)
-B.add(21)
-B.add(28)
-B.add(67)
-B.add(2)
-B.add(0)
-A.remove(0)
-print(A.contains(12))
-print(A.size())
-print(A.__iter__())
-print(A.union(B))
-print(A.intersection(B))
-print(B.difference(A))
+        return iter(self.data)
+a=Set()
+b=Set()
+a.add(1)
+a.add(3)
+a.add(4)
+a.add(5)
+b.add(4)
+b.add(3)
+print(a.intersection(b))
+print(a.difference(b))
+print(a.subset(b))
 
